4da289ee9a7e0c62552d56bca113cb5d70a94a30
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-09 12:27:25 +0530

    New things

---
+++ Tutort/Leetcode Easy/palindrome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.Leetcode_Easy
+{
+    internal class palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome..");
+        }
+    }
+}
