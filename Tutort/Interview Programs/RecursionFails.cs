9365b6578d78506b0527d761ca62b666dc4c7286
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-15 13:51:52 +0530

    Recursion Basic Problems

---
+++ Tutort/Interview Programs/RecursionFails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.Interview_Programs
+{
+    internal class RecursionFails
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Recursions(5)); ;
+        }
+
+        private static int Recursions(int n)
+        {
+            
+            if (n == 0)
+            {
+                return 0 ;
+                
+            }
+            Console.WriteLine("krishna..");
+            return Recursions(n-1);
+           
+        }
+    }
+}
