de447483776b174b71d4deedf6c2fb52190e1970
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-15 14:49:50 +0530

    Basic Recursion

---
+++ Tutort/BasicRecursion/PrintNameNtimes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.BasicRecursion
+{
+    internal class PrintNameNtimes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your n number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(NTimes(1,n));
+            Console.WriteLine(Nto1(1,n));
+        }
+
+        private static int NTimes(int i, int n)
+        {
+            if (i > n)
+            {
+                return 0;
+            }
+            Console.WriteLine("Happy 79th Independence Day..");
+            return NTimes(i + 1, n);
+        }
+
+        private static int Nto1(int i, int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            Console.WriteLine(n);
+            return Nto1(i, n-1);
+        }
+    }
+}
