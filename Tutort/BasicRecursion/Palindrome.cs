3bd2eef94bde8cbe72665dc230487e6d29434c89
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-16 21:19:54 +0530

    Dynamic Programming.

---
+++ Tutort/BasicRecursion/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.BasicRecursion
+{
+    internal class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            string s = "plaindrome";
+            Console.WriteLine(IsPalindrome(s));
+        }
+
+        private static bool IsPalindrome(string s)
+        {
+            int n = s.Length;
+            int left = 0;
+            int right = n - 1;
+            while(left < right)
+            {
+                if (s[left] !=s[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Tutort/BasicRecursion/ReverseOfAnArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.BasicRecursion
+{
+    internal class ReverseOfAnArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 4, 3, 2, 1 };
+            // Recursion(arr.Length-1,arr);
+            UsingWhileLoop(arr);
+        }
+
+        private static void Recursion(int i, int[] arr)
+        {
+            if (i < 0)
+            {
+                return;
+            }
+            Console.WriteLine(arr[i]);
+            Recursion(i - 1, arr);
+        }
+
+        private static void UsingWhileLoop(int[] arr)
+        {
+            int i = 0;
+            int j = arr.Length - 1;
+            while (i < j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+                j--;
+            }
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void SwapRecursion(int[] arr)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            if (left < right)
+            {
+                return;
+            }
+
+            
+        }
+    }
+}
