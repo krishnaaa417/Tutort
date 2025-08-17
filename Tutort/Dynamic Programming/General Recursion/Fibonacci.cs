f9071d7f063ae16a72d03bc01cb798ec71bced80
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-17 22:50:04 +0530

    recursion

---
+++ Tutort/Dynamic Programming/General Recursion/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.Dynamic_Programming.General_Recursion
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            //Console.WriteLine(NormalRecursion(n));
+            Console.WriteLine(Memoization(n));
+        }
+
+        private static int NormalRecursion(int n)
+        {
+            if(n == 0) return 0;
+            else if(n == 1) return 1;
+            return NormalRecursion(n - 1) + NormalRecursion(n-2);
+        }
+
+        private static int Memoization(int n)
+        {
+            int[] dp= new int[n+1];
+            Array.Fill(dp, -1);
+            return HelperMethod(dp, n);
+        }
+
+        private static int HelperMethod(int[] dp,int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else if (dp[n] != -1) return dp[n];
+            dp[n] = HelperMethod(dp, n - 1) + HelperMethod(dp, n - 2);
+            return dp[n];
+        }
+    }
+}
---
+++ Tutort/Dynamic Programming/General Recursion/PerfectSquares.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.Dynamic_Programming.General_Recursion
+{
+    internal class PerfectSquares
+    {
+        static void Main(string[] args)
+        {
+            int n = 12;
+            Console.WriteLine(FindPerfectSquares(n));
+        }
+       
+        private static int FindPerfectSquares(int n)
+        {
+            int[] dp = new int[n + 1];
+           
+            dp[0] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i] = i;
+
+                for (int j = 1; j * j <= i; j++)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
+        }
+
+
+    }
+}
