2b84d2c9703b982ca93cb3325f6a889b21c0d927
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-18 09:08:50 +0530

    Leetcode No 1929

---
+++ Tutort/Leetcode Easy/Concatenation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutort.Leetcode_Easy
+{
+    internal class Concatenation
+    {
+        static void Main(string[] args)
+        {
+            int[] result = { 1, 2, 3 };
+           var response = BruteForceApproach(result);
+            foreach (var item in response)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int[] BruteForceApproach(int[] result)
+        {
+            int n = result.Length;
+            int[] output = new int[2*n];
+            for (int i = 0; i < n; i++)
+            {
+                output[i] = result[i];
+                output[i + n] = result[i];
+            }
+            return output;
+        }
+    }
+}
